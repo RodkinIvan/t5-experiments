b9d2e599ac5e207ff08b3bd82c9756ada905dc24
Ivan Rodkin <irodkin2002@gmail.com>
2024-09-28 00:59:26 +0400

    fix ca bash script

--- scripts/cell_autom/finetune_ca_armt.sh
+++ scripts/cell_autom/finetune_ca_armt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export CUDA_VISIBLE_DEVICES=1,2,3,4
-NP=4 # ./test_bert_sparse_pretrain_train_valid.sh
+export CUDA_VISIBLE_DEVICES=0,1
+NP=2 # ./test_bert_sparse_pretrain_train_valid.sh
 export NCCL_ASYNC_ERROR_HANDLING=0
 set -e
 cd ../..
@@ -13,13 +13,16 @@ MEMORY_CELL=modeling_amt.language_modeling:AssociativeMemoryCell
 RECURRENT_WRAPPER=modeling_amt.language_modeling:AssociativeRecurrentWrapper
 BACKBONE_CLS=base_models.modeling_gpt_neox:GPTNeoXForCausalLM
 
-ITERS=10000
-TBS=64
+DATASET_PATH=irodkin/1dCA_r2s20T20
+
+ITERS=30000
+TBS=512
 
 MAX_N_SEGMENTSS=(10)
 MAX_VAL_SEGMENTSS=(10)
+SHIFTS=(4)
 LRS=(3e-4)
-BSS=(16)
+BSS=(128)
 
 DIM=128
 NUM_LAYERS=4
@@ -27,13 +30,12 @@ NUM_LAYERS=4
 cd base_models/gptconfigs
 python create_config.py --hidden_size $DIM --num_hidden_layers $NUM_LAYERS --num_attention_heads $NUM_LAYERS
 cd ../..
-MODEL_CFG=/home/rodkin/rmt/wip/base_models/gptconfigs/neox_tiny_${NUM_LAYERS}l${NUM_LAYERS}hd${DIM}.json
+MODEL_CFG=/home/ivan.rodkin/lab/wip/base_models/gptconfigs/neox_tiny_${NUM_LAYERS}l${NUM_LAYERS}hd${DIM}.json
 
 MEMORY_SIZE=16
 INPUT_TOKENS=20
-D_MEM=96
+D_MEM=32
 N_HEADS=1
-
 
 
 for N in 4
@@ -46,14 +48,15 @@ do
 MAX_N_SEGMENTS=${MAX_N_SEGMENTSS[j]}
 MAX_VAL_SEGMENTS=${MAX_VAL_SEGMENTSS[j]}
 
-INPUT_SIZE=$(($INPUT_TOKENS+2*$MEMORY_SIZE))
-INPUT_SEQ_LEN=$(((INPUT_SIZE-2*MEMORY_SIZE)*MAX_N_SEGMENTS))
+INPUT_SIZE=$(($INPUT_TOKENS))
+INPUT_SEQ_LEN=$(((INPUT_SIZE)*MAX_N_SEGMENTS))
 TGT_LEN=$INPUT_SEQ_LEN
 LR_=${LRS[j]}
-VAL_SEQ_LEN=$(((INPUT_SIZE-2*MEMORY_SIZE)*MAX_VAL_SEGMENTS))
+VAL_SEQ_LEN=$(((INPUT_SIZE)*MAX_VAL_SEGMENTS))
+SHIFT=${SHIFTS[j]}
 
 BS=${BSS[j]}
-K2=8
+K2=-1
 for SEGMENT_ORDERING in regular
 do
 
@@ -65,36 +68,36 @@ do
 
 if [[ j -gt 0 ]]
 then
-    PREV_SEQ_LEN=$(((INPUT_SIZE-2*MEMORY_SIZE)*${MAX_N_SEGMENTSS[j-1]}))
+    PREV_SEQ_LEN=$(((INPUT_SIZE)*${MAX_N_SEGMENTSS[j-1]}))
     MODEL_CPT=../runs/lm_long/amt/${TASK_NAME}/$MODEL_NAME/lr${LRS[j-1]}_${SCHEDULER}_dmem${D_MEM}_${PREV_SEQ_LEN}-${MAX_N_SEGMENTSS[j-1]}x${INPUT_SIZE}_mem${MEMORY_SIZE}_bs${TBS}_iters${ITERS}_${SEGMENT_ORDERING}_bptt-${K2}/run_$N 
 else
     MODEL_CPT=None
 fi
 
 echo RUNNING: TASK_NAME SRC_LEN MODEL_NAME MODEL_CLS N_SEG MEMORY_SIZE INPUT_SEQ_LEN LR N
-echo RUNNING: $TASK_NAME $SRC_LEN $MODEL_NAME $MODEL_CLS $MAX_N_SEGMENTS $MEMORY_SIZE $INPUT_SEQ_LEN $LR $N
+echo RUNNING: $TASK_NAME $SRC_LEN $MODEL_NAME $BACKBONE_CLS $MAX_N_SEGMENTS $MEMORY_SIZE $INPUT_SEQ_LEN $LR $N
 accelerate launch --num_processes $NP --config_file  ./accelerate.yaml --main_process_port 29501 run_finetuning_cell_autom.py \
         --task_name $TASK_NAME \
         --model_path ../runs/lm_long/amt/${TASK_NAME}/$MODEL_NAME/lr${LR}_${SCHEDULER}_dmem${D_MEM}_${INPUT_SEQ_LEN}-${MAX_N_SEGMENTS}x${INPUT_SIZE}_mem${MEMORY_SIZE}_bs${TBS}_iters${ITERS}_${SEGMENT_ORDERING}_bptt-${K2}/run_$N \
         --model_cfg $MODEL_CFG \
+        --dataset_path $DATASET_PATH \
         --model_type $MODEL_TYPE \
         --memory_cell_cls $MEMORY_CELL \
         --recurrent_wrapper_cls $RECURRENT_WRAPPER \
         --model_cls $BACKBONE_CLS \
         --model_cpt $MODEL_CPT \
-        --input_seq_len $INPUT_SEQ_LEN \
-        --block_size $INPUT_TOKENS \
-        --val_seq_len $VAL_SEQ_LEN \
+        --segment_size $INPUT_TOKENS \
         --input_size $INPUT_SIZE \
-        --target_seq_len $TGT_LEN \
+        --max_n_segments $MAX_N_SEGMENTS \
         --num_mem_tokens $MEMORY_SIZE \
-        --train_timesteps $MAX_N_SEGMENTS \
-        --valid_timesteps $MAX_VAL_SEGMENTS \
+        --num_timesteps $MAX_N_SEGMENTS \
+        --num_test_timesteps $MAX_VAL_SEGMENTS \
+        --prediction_shift $SHIFT \
+        --optimize_metric exact_match --optimize_mode max \
         --batch_size $BS \
         --gradient_accumulation_steps $(($TBS/$BS/$NP)) \
         --iters $ITERS \
         --num_training_steps $(($ITERS*2))\
-        --k1 -1 --k2 $K2 \
         --optimizer AdamW  --weight_decay 0.01 \
         --lr ${LR} --lr_scheduler $SCHEDULER --num_warmup_steps 1000 \
         --data_n_workers 2 \
@@ -102,10 +105,9 @@ accelerate launch --num_processes $NP --config_file  ./accelerate.yaml --main_pr
         --show_valid_examples 5 \
         --early_stopping_patience 15 \
         --seed $(($N+42*$j)) \
-        --clip_grad_value 5.0 \
+        --clip_grad_value 1.0 \
         --save_best \
         --d_mem $D_MEM \
-        --n_heads $N_HEADS \
         --layers_attr gpt_neox.layers
 done
 done
