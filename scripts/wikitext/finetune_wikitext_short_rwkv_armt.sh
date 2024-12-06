b6e8d23252e85a2235cfc47c8f9afed6028ef745
Ivan Rodkin <ivan.rodkin@mbzuai.ac.ae>
2024-12-06 17:13:00 +0400

    update rwkv_armt wikitext script and add eval script for rwkv armt on babilong

--- scripts/wikitext/finetune_wikitext_short_rwkv_armt.sh
+++ scripts/wikitext/finetune_wikitext_short_rwkv_armt.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=1
 export RWKV_NO_CUDA=1
 export RWKV_JIT_ON=0
 export WANDB_PROJECT=t5-experiments
 export RWKV_ARMT=1
-NP=2
+NP=$(echo $CUDA_VISIBLE_DEVICES | awk -F',' '{print NF}')
 set -e
 cd ../..
 
@@ -29,7 +29,7 @@ TBS=64
 MAX_N_SEGMENTSS=(8)
 MAX_VAL_SEGMENTSS=(16)
 LRS=(1e-4)
-BSS=(2)
+BSS=(4)
 
 
 MODEL_NAME=~/lab/rwkv-x060-173m-pile-20240515-ctx4k.pth
@@ -114,8 +114,8 @@ accelerate launch --num_processes $NP --config_file  ./accelerate.yaml --main_pr
         --tokenizer $TOKENIZER \
         --d_mem $D_MEM \
         --n_heads $N_HEADS \
-        --layers_attr model.blocks \
-        --no_denom
+        --layers_attr model.blocks
+        # --no_denom
 done
 done
 done
